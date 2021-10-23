ae743c3aef729688c1e58f484e7ef27c3a41e019
bgordon8 <gordonbrian16@gmail.com>
2021-10-23 10:20:03 -0600

    Adds db config connection

---
+++ knexfile.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+
+const defaultOptions = {
+  client: 'postgresgl',
+  connection: {
+    database: 'new_build_01',
+    host: 'localhost',
+    port: 5432,
+    user: '',
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: path.join(__dirname, 'src', 'db', 'migrations'),
+  },
+  seeds: {
+    directory: path.join(__dirname, 'src', 'db', 'seeds'),
+  },
+};
+
+export default {
+  development: config({
+    connection: {
+      database: 'new_build_01',
+    },
+  }),
+  test: config({
+    connection: {
+      database: 'new_build_01_test',
+    },
+  }),
+  production: {},
+} as { [key: string]: object };
+
+function config(overides: any) {
+  return Object.assign({}, defaultOptions, overides);
+}
